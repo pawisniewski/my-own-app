988c0dfb4a05de38eeb6290079d425badcb0dcc6
Pawel Wisniewski <pawel_wisniewski@outlook.com>
2021-06-30 18:12:25 +0200

    Add basic server code

--- README.md
+++ README.md
@@ -14,6 +14,10 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
 
+### `yarn start-server`
+
+Starts local server with nodemon on default port 8000 or port defined in process env.
+
 ### `yarn test`
 
 Launches the test runner in the interactive watch mode.\
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+/* MIDDLEWARE */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+/* API ENDPOINTS */
+//app.use('/api', routes);
+
+/* API ERROR PAGES */
+app.use('/api', (req, res) => {
+  res.status(404).send({ post: 'Not found...' });
+});
+
+/* REACT WEBSITE */
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+/* START SERVER */
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log('Server is running on port: ' + port);
+});
